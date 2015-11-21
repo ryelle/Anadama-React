dbe81b2e80d3f9c196e0cfdf252a96d55e8d0b25
Kelly Dwan <kelly.dwan@gmail.com>
2015-11-21 15:50:16 -0500

    Build the navigation in JS too

--- header.php
+++ header.php
@@ -38,10 +38,7 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'anadama' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<nav id="site-navigation" class="main-navigation" role="navigation"></nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
