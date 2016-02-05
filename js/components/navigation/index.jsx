8f73f91b375258c9451b86f026f407e4fd42801b
Kelly Dwan <kelly.dwan@gmail.com>
2016-02-05 11:56:35 -0500

    Merge in menu changes from Foxhound: adds a11y focus class, and adds a highlight class if we detect we’re on the current menu item

--- js/components/navigation/index.jsx
+++ js/components/navigation/index.jsx
@@ -6,15 +6,6 @@ import classNames from 'classnames';
 import API from 'utils/api';
 import NavigationStore from '../../stores/navigation-store';
 
-/**
- * Method to retrieve state from Stores
- */
-function getState() {
-	return {
-		data: NavigationStore.getMenu(),
-	};
-}
-
 let SubMenu = React.createClass( {
 	render: function() {
 		let menu = this.props.items.map( function( item, i ) {
@@ -30,20 +21,48 @@ let SubMenu = React.createClass( {
 } );
 
 let MenuItem = React.createClass( {
+	blur: function( event ) {
+		event.target.blur();
+	},
+
+	toggleFocus: function( event ) {
+		var self = event.target;
+
+		// Move up through the ancestors of the current link until we hit .nav-menu.
+		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+			// On li elements toggle the class .focus.
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				if ( -1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
+				} else {
+					self.className += ' focus';
+				}
+			}
+
+			self = self.parentElement;
+		}
+	},
+
 	render: function() {
+		let re;
+		if ( location.pathname !== '/' ) {
+			re = new RegExp( location.pathname + '$' );
+		} else {
+			re = new RegExp( location.hostname + '/$' );
+		}
 		let classes = classNames( {
 			'menu-item': true,
 			'menu-item-has-children': this.props.item.children.length,
-			'current-menu-item': false,
+			'current-menu-item': ( location.pathname === this.props.item.url ) || re.test( this.props.item.url ),
 			'current-menu-ancestor': false,
 			'current-menu-parent': false,
-		} );
+		}, this.props.item.classes );
 
 		return (
-			<li className={ classes }>
-				<a href={ this.props.item.url }>{ this.props.item.title }</a>
+			<li className={ classes } aria-haspopup={ this.props.item.children.length > 0 }>
+				<a href={ this.props.item.url } onClick={ this.blur } onFocus={ this.toggleFocus } onBlur={ this.toggleFocus }>{ this.props.item.title }</a>
 				{ this.props.item.children.length ?
-					<SubMenu items={ this.props.item.children } />:
+					<SubMenu items={ this.props.item.children } /> :
 					null
 				}
 			</li>
@@ -51,14 +70,24 @@ let MenuItem = React.createClass( {
 	}
 } );
 
+/*
+ * Method to retrieve state from Stores
+ */
+function getState() {
+	return {
+		data: NavigationStore.getMenu(),
+		isMenuOpen: false,
+	};
+}
+
 let Navigation = React.createClass( {
 	getInitialState: function() {
 		return getState();
 	},
 
 	componentDidMount: function() {
-		NavigationStore.addChangeListener( this._onChange );
 		API.getMenu( '/menu-locations/primary/' );
+		NavigationStore.addChangeListener( this._onChange );
 	},
 
 	componentWillUnmount: function() {
@@ -69,13 +98,26 @@ let Navigation = React.createClass( {
 		this.setState( getState() );
 	},
 
+	toggleMenu: function( event ) {
+		event.preventDefault();
+		this.setState( { isMenuOpen: ! this.state.isMenuOpen } );
+	},
+
 	render: function() {
 		let menu = this.state.data.map( function( item, i ) {
 			return <MenuItem item={ item } key={ i } />
 		} );
 
+		let menuClasses = classNames( {
+			'menu-container': true,
+			'menu-open': this.state.isMenuOpen,
+		} );
+
 		return (
-			<div className="menu-container">
+			<div className={ menuClasses }>
+				<div className="menu-toggle" onClick={ this.toggleMenu }>
+					<button onClick={ this.toggleMenu }>Menu</button>
+				</div>
 				<ul className="menu nav-menu" aria-expanded="false">
 					{ menu }
 				</ul>
