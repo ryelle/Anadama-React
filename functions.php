71279586a9ec6ca1c642ceed054a9998be2b4f60
Weston Ruter <weston@xwp.co>
2016-10-30 23:32:22 -0700

    Add workaround for issue in WP API Menus plugin

--- functions.php
+++ functions.php
@@ -200,5 +200,23 @@ add_action( 'after_setup_theme', 'anadama_jetpack_setup' );
 function anadama_customize_register( WP_Customize_Manager $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport        = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+
+	add_filter( 'wp_get_nav_menu_items', '_anadama_filter_wp_api_nav_menu_items_workaround', 20  );
 }
 add_action( 'customize_register', 'anadama_customize_register' );
+
+/**
+ * Workaround issue in WP API Menus plugin to force nav menu item classes to be arrays instead of strings.
+ *
+ * @see \WP_REST_Menus::get_menu_location()
+ *
+ * @param array $items Nav menu items.
+ */
+function _anadama_filter_wp_api_nav_menu_items_workaround( $items ) {
+	foreach ( $items as &$item ) {
+		if ( is_string( $item->classes ) ) {
+			$item->classes = explode( ' ', $item->classes );
+		}
+	}
+	return $items;
+}
