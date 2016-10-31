6eb93317d4f246fff935166d7ebd2b41dbd05d6d
Weston Ruter <weston@xwp.co>
2016-10-30 22:19:24 -0700

    Override customizer preview handleLinkClick to allow history.pushState to do its thing

--- js/customize-preview.js
+++ js/customize-preview.js
@@ -15,4 +15,32 @@
 		} );
 	} );
 
+	/**
+	 * Override the handler for clicking links in preview to allow history.pushState() to do its thing.
+	 *
+	 * @param {jQuery.Event} event Event.
+	 */
+	api.Preview.prototype.handleLinkClick = function handleLinkClick( event ) {
+		var link, isInternalJumpLink;
+		link = $( event.target );
+
+		// No-op if the anchor is not a link.
+		if ( _.isUndefined( link.attr( 'href' ) ) ) {
+			return;
+		}
+
+		isInternalJumpLink = ( '#' === link.attr( 'href' ).substr( 0, 1 ) );
+
+		// Allow internal jump links to behave normally without preventing default.
+		if ( isInternalJumpLink ) {
+			return;
+		}
+
+		// If the link is not previewable, prevent the browser from navigating to it.
+		if ( ! api.isLinkPreviewable( link[0] ) ) {
+			wp.a11y.speak( api.settings.l10n.linkUnpreviewable );
+			event.preventDefault();
+		}
+	};
+
 } )( jQuery, wp.customize );
